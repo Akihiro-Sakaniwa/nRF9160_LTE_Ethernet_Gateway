ea99d13947bc23af4b8cc20a85565bbc8fae023d
Akihiro-Sakaniwa <a.sakaniwa@nifty.com>
2024-04-28 11:27:58 +0900

    1st commit

--- build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "sparkfun_thing_plus_nrf9160_ns" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "/opt/nordic/ncs/v2.5.2/zephyr;/opt/nordic/ncs/v2.5.2/nrf;/opt/nordic/ncs/v2.5.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "/Users/sakaniwa/work/nRF9160/LTE_ETH_GW/SINGLEHEAD_LTE_ETH_GW/nrf9160_lte_ethernet_gateway/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/Users/sakaniwa/work/nRF9160/LTE_ETH_GW/SINGLEHEAD_LTE_ETH_GW/nrf9160_lte_ethernet_gateway/sparkfun_thing_plus_nrf9160_ns.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/opt/nordic/ncs/toolchains/20d68df7e5/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
