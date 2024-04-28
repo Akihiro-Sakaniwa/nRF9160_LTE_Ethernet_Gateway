b9f6600900a59ccb943599255e0b531010309588
Akihiro-Sakaniwa <a.sakaniwa@nifty.com>
2024-04-28 11:39:02 +0900

    1st commit

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,26 @@
+empty_file.o: /opt/nordic/ncs/v2.5.2/zephyr/misc/empty_file.c \
+ /opt/nordic/ncs/v2.5.2/zephyr/boards/arm/sparkfun_thing_plus_nrf9160/sparkfun_thing_plus_nrf9160_ns.dts \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/nordic/nrf9160ns_sica.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/common/mem.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/nordic/nrf91ns.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/armv8-m.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/common/skeleton.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/sys/util_macro.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/sys/util_internal.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/sys/util_loops.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/common/freq.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/nordic/override.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/dts/arm/nordic/nrf91_peripherals.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/boards/arm/sparkfun_thing_plus_nrf9160/sparkfun_thing_plus_nrf9160_common.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/boards/arm/sparkfun_thing_plus_nrf9160/sparkfun_thing_plus_nrf9160_common-pinctrl.dtsi \
+ /opt/nordic/ncs/v2.5.2/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /opt/nordic/ncs/v2.5.2/zephyr/boards/arm/sparkfun_thing_plus_nrf9160/sparkfun_thing_plus_nrf9160_partition_conf.dtsi \
+ /Users/sakaniwa/work/nRF9160/LTE_ETH_GW/SINGLEHEAD_LTE_ETH_GW/nrf9160_lte_ethernet_gateway/sparkfun_thing_plus_nrf9160_ns.overlay
---
+++ src/arp_cache.h
@@ -0,0 +1,56 @@
+#ifndef __ARP_H__
+#define __ARP_H__
+
+#include <zephyr/kernel.h>
+#include <zephyr/net/net_pkt.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_l2.h>
+#include <zephyr/net/net_ip.h>
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_context.h>
+
+#define ARP_CACHE_TIMEOUT_MS (20*60*1000)
+#define MAX_ARP_CACHE_ROWS (96)
+
+typedef struct __arp_cache_row_t
+{
+    uint32_t ip;
+    uint8_t  mac[6];
+    uint32_t ts;
+} arp_cache_row_t;
+
+typedef struct __arp_cache_tbl_t
+{
+    arp_cache_row_t  row[MAX_ARP_CACHE_ROWS];
+} arp_cache_tbl_t;
+
+typedef struct __pkt_arp_t
+{
+    uint8_t     eth_dst_mac[6];
+    uint8_t     eth_src_mac[6];
+    uint8_t     frame_type[2];
+
+    uint8_t     htype[2];
+    uint8_t     ptype[2];
+    uint8_t     hsz[1];
+    uint8_t     psz[1];
+    uint8_t     operation[2];
+    uint8_t     src_mac[6];
+    uint8_t     src_ip[4];
+    uint8_t     dst_mac[6];
+    uint8_t     dst_ip[4];
+    uint8_t     padding[18];
+} pkt_arp_t;
+
+extern arp_cache_tbl_t arp_cache;
+
+void operate_packet_arp(uint8_t *buffer, uint32_t length);
+
+void gc_arp_cache(uint32_t timeout_ms);
+arp_cache_row_t* search_arp_cache_empty(void);
+arp_cache_row_t* search_arp_cache_by_ip(uint32_t tgt_ip);
+arp_cache_row_t* update_arp_cache_by_ip(uint32_t tgt_ip, uint8_t* mac);
+bool resolve_arp(uint32_t tgt_ip, uint8_t *mac, uint32_t timeout_ms, bool use_arp_cache);
+
+
+#endif  // __ARP_H__
